cbcbabc26a682ceb72a3a8c6c0c1e917b7ceae8d
zola2311 <zola2311@gmail.com>
2022-06-20 12:54:56 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,10 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except:
+        except (ValueError,TypeError,IndexError):
             break
         else:
-            count ++
+            count +=1
 
     print()
     return (count)
